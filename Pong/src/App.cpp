ab54a1fd2cc24403e838971a9c61a73677bcce59
Hatrickek <halimbayka@outlook.com>
2025-10-12 20:29:47 +0300

    update for latest oxylus

--- Pong/src/App.cpp
+++ Pong/src/App.cpp
@@ -1,39 +1,40 @@
-#include <Core/EntryPoint.hpp>
+#include <Asset/AssetManager.hpp>
 #include <Core/App.hpp>
+#include <Core/EntryPoint.hpp>
+#include <Core/Enum.hpp>
+#include <Core/Input.hpp>
+#include <Networking/NetworkManager.hpp>
+#include <Physics/Physics.hpp>
+#include <Scripting/LuaManager.hpp>
 
 #include "GameLayer.hpp"
-
-#include <filesystem>
 
 namespace ox {
 class PongApp : public ox::App {
 public:
-  PongApp(const ox::AppSpec& spec) : App(spec) { }
+  PongApp() : App() {}
 };
 
 App* create_application(const AppCommandLineArgs& args) {
-  AppSpec spec;
-  spec.name = "Pong";
-  spec.working_directory = std::filesystem::current_path().string();
-  spec.command_line_args = args;
-  spec.assets_path = "Assets";
-  spec.headless = false;
-  const WindowInfo::Icon icon = { };
-  spec.window_info = {
-	  .title = spec.name,
-	  .icon = icon,
-	  .width = 1720,
-	  .height = 900,
-#ifdef OX_PLATFORM_LINUX
-	  .flags = WindowFlag::Centered,
-#else
-	  .flags = WindowFlag::Centered | WindowFlag::Resizable,
-#endif
-  };
-
-  const auto app = new PongApp(spec);
-  app->push_layer(std::make_unique<rog::GameLayer>());
+  const auto app = new PongApp();
+  app->with_name("Pong")
+    .with_args(args)
+    .with_window({
+      .title = "Pong",
+      .icon = {},
+      .width = 1720,
+      .height = 900,
+      .flags = WindowFlag::Centered | WindowFlag::Resizable,
+    })
+    .with<AssetManager>()
+    .with<AudioEngine>()
+    .with<Physics>()
+    .with<Input>()
+    .with<NetworkManager>()
+    .with<LuaManager>()
+    .push_imgui_layer()
+    .push_layer(std::make_unique<rog::GameLayer>());
 
   return app;
 }
-}
+} // namespace ox
--- Pong/src/GameLayer.cpp
+++ Pong/src/GameLayer.cpp
@@ -20,10 +20,11 @@ void GameLayer::on_attach() {
   ZoneScoped;
 
   const auto* app = ox::App::get();
-  auto* vfs = app->get_vfs();
-  auto* asset_man = app->get_asset_manager();
+  auto& vfs = app->get_vfs();
 
-  auto scenes_dir = vfs->resolve_physical_dir(ox::VFS::APP_DIR, "Scenes");
+  auto scenes_dir = vfs.resolve_physical_dir(ox::VFS::APP_DIR, "Scenes");
+
+#if 0
   auto models_dir = vfs->resolve_physical_dir(ox::VFS::APP_DIR, "Models");
   auto scripts_dir = vfs->resolve_physical_dir(ox::VFS::APP_DIR, "Scripts");
 
@@ -31,6 +32,7 @@ void GameLayer::on_attach() {
   asset_man->import_asset(models_dir + "/player.glb.oxasset");
   asset_man->import_asset(scripts_dir + "/camera.lua.oxasset");
   asset_man->import_asset(scripts_dir + "/scene.lua.oxasset");
+#endif
 
   main_scene = std::make_unique<ox::Scene>("MainScene");
   main_scene->load_from_file(scenes_dir + "/main_scene.oxscene");
@@ -65,7 +67,6 @@ void GameLayer::on_render(vuk::Extent3D extent, vuk::Format format) {
     const ox::Renderer::RenderInfo render_info = {
         .extent = extent,
         .format = format,
-        .picking_texel = {},
     };
     auto scene_view_image = renderer_instance->render(render_info);
 
